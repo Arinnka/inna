687696256d06f8e4771f569655216611aa589eff
Arinnka <Delena315@yandex.ru>
2017-01-25 20:53:18 +0300

    важно

---
+++ lab 5-6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab_5_6.Startup))]
+namespace lab_5_6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
